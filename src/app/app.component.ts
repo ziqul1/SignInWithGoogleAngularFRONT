1f6a1cc47e23a1b81c220cd620c292330013d7d5
Maciej Taszarek <maciej.taszarek@billennium.com>
2022-07-29 12:04:39 +0200

    Logging working properly with bootstrap view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SocialUser, SocialAuthService } from '@abacritt/angularx-social-login';
 import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,23 +9,40 @@ import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'SignInWithGoogleAngularFRONT';
 
- user: SocialUser | null;
+  user: SocialUser | null;
+  hasApiAccess = false;
 
-  constructor(private authService: SocialAuthService) {
+  constructor(private authService: SocialAuthService, private http: HttpClient) {
     this.user = null;
     this.authService.authState.subscribe((user: SocialUser) => {
       console.log(user);
+      if (user) {
+        this.http.post<any>('https://localhost:5001/user/authenticate', { idToken: user.idToken }).subscribe((authToken: any) => {
+          console.log(authToken);
+
+          let reqHeader = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + authToken.authToken
+          });
+          this.http.get<any>('https://localhost:5001/secured', { headers: reqHeader }).subscribe((data: any) => {
+            this.hasApiAccess = true;
+          })
+        })
+      }
       this.user = user;
     });
   }
 
   signInWithGoogle(): void {
-   this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then((x: any) => console.log(x));
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then((x: any) => console.log(x));
   }
 
   signOut(): void {
-   this.authService.signOut();
+    this.authService.signOut();
+    this.hasApiAccess = false;
   }
+
 } 
